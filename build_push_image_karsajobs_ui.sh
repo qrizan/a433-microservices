7686fc9b3e730003dbcbb072e4e7c32904a5ec8e
qrizan <qrizan@gmail.com>
2023-06-01 22:41:27 +0700

    add kubernetes configuration

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# build image dari Dockerfile
 docker build -t ghcr.io/qrizan/karsajobs-ui:latest .
 
+# login ke Github container registry
 echo $GITHUB_TOKEN | docker login ghcr.io -u qrizan --password-stdin
 
+# push image hasil build ke github packages
 docker push ghcr.io/qrizan/karsajobs-ui:latest
